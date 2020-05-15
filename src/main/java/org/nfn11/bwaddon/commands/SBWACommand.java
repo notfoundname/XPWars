8e07d8d74aac05eb355699b02a332622dbe63076
notfoundname <48317169+notfoundname@users.noreply.github.com>
2020-05-16 03:04:16 +0800

    Update SBWACommand.java

--- src/main/java/org/nfn11/bwaddon/commands/SBWACommand.java
+++ src/main/java/org/nfn11/bwaddon/commands/SBWACommand.java
@@ -22,27 +22,22 @@ public class SBWACommand extends BaseCommand {
 	@Override
 	public boolean execute(CommandSender sender, List<String> args) {
 		if (args.size() >= 1) {
-			switch (args.get(1)) {
-				case "reload":
-					if (!sender.hasPermission(ADMIN_PERMISSION)) {
-						sender.sendMessage(BwAddon.getConfigurator().config.getString("messages.commands.reloaded".replace("&", "§")));
-						break;
-					}
-					Bukkit.getServer().getPluginManager().disablePlugin(BwAddon.getInstance());
-					Bukkit.getServer().getPluginManager().enablePlugin(BwAddon.getInstance());
-					if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-						new org.nfn11.bwaddon.placeholderapi.PlaceholderAPIHook(BwAddon.getInstance()).register();
-						Bukkit.getLogger().info("Succesfully registered PlaceholderAPI!");
-					}
+			if (args.get(1).equals("reload")) {
+				if (!sender.hasPermission(ADMIN_PERMISSION)) {
 					sender.sendMessage(BwAddon.getConfigurator().config.getString("messages.commands.reloaded".replace("&", "§")));
-					break;
-				default:
-					sender.sendMessage(BwAddon.getConfigurator().config.getString("messages.commands.unknown".replace("&", "§")));
-					break;
+					return true;
+				}
+				Bukkit.getServer().getPluginManager().disablePlugin(BwAddon.getInstance());
+				Bukkit.getServer().getPluginManager().enablePlugin(BwAddon.getInstance());
+				if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+					new org.nfn11.bwaddon.placeholderapi.PlaceholderAPIHook(BwAddon.getInstance()).register();
+					Bukkit.getLogger().info("Succesfully registered PlaceholderAPI!");
+				}
+				sender.sendMessage(BwAddon.getConfigurator().config.getString("messages.commands.reloaded".replace("&", "§")));
+			} else {
+				sender.sendMessage(BwAddon.getConfigurator().config.getString("messages.commands.unknown".replace("&", "§")));
 			}
-			
 		}
-		
 		return true;
 	}
 
