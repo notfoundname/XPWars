497dcd57386de4627ae1af402e1389c4eaf07553
notfoundname <48317169+notfoundname@users.noreply.github.com>
2020-05-31 15:55:00 +0800

    Update LevelShop.java

--- src/main/java/org/nfn11/bwaddon/inventories/LevelShop.java
+++ src/main/java/org/nfn11/bwaddon/inventories/LevelShop.java
@@ -23,7 +23,6 @@ import org.screamingsandals.bedwars.game.Game;
 import org.screamingsandals.bedwars.game.GamePlayer;
 import org.screamingsandals.bedwars.utils.Debugger;
 import org.screamingsandals.bedwars.utils.Sounds;
-import org.nfn11.misat11.lib.Debug;
 import org.screamingsandals.simpleinventories.SimpleInventories;
 import org.screamingsandals.simpleinventories.events.GenerateItemEvent;
 import org.screamingsandals.simpleinventories.events.PreActionEvent;
@@ -38,9 +37,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static org.nfn11.misat11.lib.I18n.i18n;
-import static org.nfn11.misat11.lib.I18n.i18nonly;
 
 public class LevelShop implements Listener {
 	/*
